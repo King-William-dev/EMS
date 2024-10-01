d1f8200c1ffd437e077faefe853c1c853de7bb82
King William <seanapowi@gmail.com>
2024-10-01 21:31:43 +0300

    second commit

--- server/db/db.js
+++ server/db/db.js
@@ -2,9 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      'mongodb+srv://seanapowi:test123@cluster0.r9ggb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-    );
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
