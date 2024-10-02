9d1b24aed90d69d3e8834803a8378051578ac571
William Olei <seanapowi@gmail.com>
2024-10-02 05:29:46 +0300

    third commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import AdminDashboard from './pages/AdminDashboard';
+
 function App() {
-  return <div className="text-3xl text-teal-500">Welcome to EMS</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/admin-dashboard" />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/admin-dashboard" element={<AdminDashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        'http://localhost:5000/api/auth/login',
+        {
+          email,
+          password,
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="flex flex-col items-center h-screen justify-center bg-gradient-to-b from-teal-600 from-50% to-gray-100 to-50% space-y-6">
+      <h2 className="font-poppins text-3xl text-white">Edulink </h2>
+      <div className="border shadow p-6 w-80 bg-white">
+        <h2 className="text-2xl font-bold mb-4">Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700">
+              Email
+            </label>
+            <input
+              onChange={e => setEmail(e.target.value)}
+              className="w-full px-3 py-2 border"
+              type="email"
+              name="email"
+              placeholder="Enter Email"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700" htmlFor="password">
+              Password
+            </label>
+            <input
+              onChange={e => setPassword(e.target.value)}
+              className="w-full px-3 py-2 border"
+              type="password"
+              name="password"
+              placeholder="Enter Password"
+            />
+          </div>
+          <div className="mb-4 flex items-center justify-between">
+            <label className="inline-flex items-center">
+              <input type="checkbox" className="form-checkbox   " />
+              <span className="ml-2 text-gray-700">Remember Me</span>
+            </label>
+            <a href="#" className="text-teal-600">
+              Forgot Password
+            </a>
+          </div>
+          <div className="mb-4">
+            <button
+              type="submit"
+              className="w-full bg-teal-600 text-white py-2 rounded -5"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
